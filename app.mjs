599303d649920b1f49e90a205f32ec648091f8bd
오지환 <ovitwisdom@gmail.com>
2025-04-29 02:00:23 +0900

    503 에러 수정

--- app.mjs
+++ app.mjs
@@ -1,15 +1,15 @@
 import express from "express";
-import cors from 'cors';
-import session from 'express-session';
+import cors from "cors";
+import session from "express-session";
 import postsRouter from "./router/posts.mjs"
 import authRouter from "./router/auth.mjs"
 import { config } from "./config.mjs"
 
 const app = express();
 
-app.use(cors()); // cors 허용 추가
-
+app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use(session({
   secret: '!@#$%^&*()',
@@ -28,4 +28,5 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 })
 
-app.listen(config.host.port, () => console.log('Server is running!'));
+const PORT = process.env.PORT || config.host.port
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
