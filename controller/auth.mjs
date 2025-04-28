a2944e3fea08ee6aff5abdb2cf2cb000480188ad
오지환 <ovitwisdom@gmail.com>
2025-04-29 03:21:52 +0900

    에휴

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -32,10 +32,11 @@ export async function signup(req, res, next){
 export async function login(req, res, next){
 	const {userid, password} = req.body;
 	const user = await authRepository.findByUserId(userid);
+	
 	if(!user) {
-	  req.session.userId = user.userid;
-	  res.status(401).json(`${userid} 아이디를 찾을 수 없음`);
+	  return res.status(401).json({ message: `${userid} 아이디를 찾을 수 없음` });
 	}
+
 	const isValidPassword = await bcrypt.compare(password, user.password)
 	if (!isValidPassword) {
 		return res.status(401).json({message:"아이디 또는 비밀번호 확인"})
