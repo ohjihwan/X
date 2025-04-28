ed0e2c100188742853ab73df5934a9d9b6b4d745
오지환 <ovitwisdom@gmail.com>
2025-04-29 01:31:26 +0900

    깃허브 올리기

--- app.mjs
+++ app.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from 'cors';
 import session from 'express-session';
 import postsRouter from "./router/posts.mjs"
 import authRouter from "./router/auth.mjs"
 import { config } from "./config.mjs"
 
 const app = express();
+
+app.use(cors()); // cors 허용 추가
 
 app.use(express.json());
 
